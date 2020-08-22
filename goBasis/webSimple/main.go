26949c04534d933896cebcca8dbf9add42cbc4c6
yiming <1645398@qq.com>
2020-08-23 03:05:18 +0800

    html

--- goBasis/webSimple/main.go
+++ goBasis/webSimple/main.go
@@ -3,20 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 func myWeb(w http.ResponseWriter, r *http.Request) {
+	/*
+		r.ParseForm()
+		for k, v := range r.URL.Query() {
+			fmt.Println("key:", k, ",value:", v[0])
+		}
+		for k, v := range r.PostForm {
+			fmt.Println("key:", k, ", value:", v[0])
+		}
+		fmt.Fprintf(w, "this is new begin")
+	*/
 
-	r.ParseForm()
-	for k, v := range r.URL.Query() {
-		fmt.Println("key:", k, ",value:", v[0])
+	//	t := template.New("index")
+	//	t.Parse("<div id='teamlateTextDiv'>Hi,{{.name}},{{.someStr}}</div>")
+
+	t, _ := template.ParseFiles("./templates/index.html")
+	data := map[string]string{
+		"name":    "zhangsan",
+		"someStr": "this is string",
 	}
-
-	for k, v := range r.PostForm {
-		fmt.Println("key:", k, ", value:", v[0])
-	}
-
-	fmt.Fprintf(w, "this is new begin")
+	t.Execute(w, data)
 }
 
 func main() {
