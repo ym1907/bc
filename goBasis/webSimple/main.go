5565838387df01913d8f54b89cdbdc721b6354c8
yiming <1645398@qq.com>
2020-08-22 23:12:23 +0800

    ParseForm

--- goBasis/webSimple/main.go
+++ goBasis/webSimple/main.go
@@ -6,6 +6,16 @@ import (
 )
 
 func myWeb(w http.ResponseWriter, r *http.Request) {
+
+	r.ParseForm()
+	for k, v := range r.URL.Query() {
+		fmt.Println("key:", k, ",value:", v[0])
+	}
+
+	for k, v := range r.PostForm {
+		fmt.Println("key:", k, ", value:", v[0])
+	}
+
 	fmt.Fprintf(w, "this is new begin")
 }
 
