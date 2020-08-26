b504f8003baa47642e29516f110ecb3a4e527dc6
yiming <1645398@qq.com>
2020-08-26 21:29:05 +0800

    unjson

---
+++ goBasis/jsonoGo/structJson/unMarshal/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+	strs := People{
+		Name: "zhangsan",
+		Addr: "beijing",
+		Age:  18,
+	}
+	fmt.Println("data:  ", strs)
+
+	dataJson, err := json.Marshal(strs)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("json: %v\n", dataJson)
+
+	var dataUnJson interface{}
+	err = json.Unmarshal(dataJson, &dataUnJson)
+	fmt.Printf("unjson: %v\n", dataUnJson)
+
+}
+
+type People struct {
+	Name string
+	Addr string
+	Age  int
+}
