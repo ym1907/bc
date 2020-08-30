04291a78f553d77045256edfd90821537cd22231
Harry-wh <1093876434@qq.com>
2020-08-30 16:34:06 +0800

    1

--- javaBasis/dataStructure/src/DataBasics/Demo04Node.java
+++ javaBasis/dataStructure/src/DataBasics/Demo04Node.java
@@ -2,4 +2,13 @@ package DataBasics;
 
 public class Demo04Node {
 
+    private int num;
+    private Demo04Node node;
+
+    public Demo04Node(int num){
+        this.num = num;
+    }
+
+
+
 }
---
+++ javaBasis/dataStructure/src/DataBasics/Demo04Test.java
@@ -0,0 +1,8 @@
+package DataBasics;
+
+public class Demo04Test {
+    public static void main(String[] args) {
+        Demo04Node node = new Demo04Node(10);
+
+    }
+}
