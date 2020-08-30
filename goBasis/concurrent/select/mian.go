71b3e61447ed0c01abe05858dc6d9e6fccf2751c
yiming <1645398@qq.com>
2020-08-30 23:48:04 +0800

    structure

--- goBasis/concurrent/select/mian.go
+++ goBasis/concurrent/select/mian.go
@@ -7,6 +7,7 @@ func fibonacci(c, quit chan int) {
 	for {
 		select {
 		case c <- x:
+			fmt.Println("c <- x :", x)
 			x, y = y, x+y
 		case <-quit:
 			fmt.Println("quit")
@@ -24,5 +25,6 @@ func main() {
 		}
 		quit <- 0
 	}()
+
 	fibonacci(c, quit)
 }
