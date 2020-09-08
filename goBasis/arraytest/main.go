2e8d9abdc4f953461d7154992ef7e673a5adad10
yiming <1645398@qq.com>
2020-09-08 23:57:14 +0800

    array

---
+++ goBasis/arraytest/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+
+	var a []int
+	for i := 5; i < 10; i++ {
+		//	fmt.Println(i)
+		a = append(a, i)
+		//	fmt.Println("a", a)
+	}
+	fmt.Println("a", a)
+
+	b := make([]int, 0)
+	for x := 5; x < 10; x++ {
+		b = append(b, x)
+	}
+	fmt.Println("b", b)
+}
