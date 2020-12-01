af3e22f2916ba4052dfcb1d8a0168ccef3712b07
yiming company <1645398@qq.com>
2020-12-01 14:26:07 +0800

    webhttp

--- goBasis/webhttp/main.go
+++ goBasis/webhttp/main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
 
-func main() {
-	fmt.Println("vim-go")
+func sayHelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello astaxie!")
 }
 
-func printHello(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
+func main() {
+	http.HandleFunc("/", sayHelloName)
+	err := http.ListenAndServe(":9090", nil) //设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 
 }
