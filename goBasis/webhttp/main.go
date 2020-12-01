0ae338e613d48fc09194cf347108a6938add8e21
yiming company <1645398@qq.com>
2020-12-01 14:49:03 +0800

    webhttp

--- goBasis/webhttp/main.go
+++ goBasis/webhttp/main.go
@@ -21,6 +21,7 @@ func sayHelloName(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	fmt.Println("webhttp")
 	http.HandleFunc("/", sayHelloName)
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 	if err != nil {
