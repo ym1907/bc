07ebf6458f0e8d1180c157bd9d1f3838ee075cda
Harry-wh <1093876434@qq.com>
2021-01-01 20:36:52 +0800

    21-1-1

--- javaBasis/dataStructure/src/DataBasics/Demo04Node.java
+++ javaBasis/dataStructure/src/DataBasics/Demo04Node.java
@@ -11,18 +11,25 @@ public class Demo04Node {
         this.num = num;
     }
 
+//    在当前节点后追加节点
+    public void add(Demo04Node node){
+        Demo04Node oldNode = this.node;
+        this.node = node;
+        node.node = oldNode;
+    }
 
+//    在链表末端追加节点
     public Demo04Node append(Demo04Node node){
         //当前节点
         Demo04Node currentNode = this;
         //获取到最后一个节点
         while(currentNode.hasNext()){
-            //取出下一个节点
+            if(node.equals(currentNode)){
+                throw new RuntimeException("当前节点已存在");
+            }
             Demo04Node nextNode = currentNode.next();
-            //赋值给当前节点
             currentNode = nextNode;
         }
-        //追加节点
         currentNode.node = node;
         return this;
     }
@@ -33,12 +40,6 @@ public class Demo04Node {
 
     public Demo04Node next(){
         return this.node;
-    }
-
-    public void add(Demo04Node node){
-        Demo04Node oldNode = this.node;
-        this.node = node;
-        node.node = oldNode;
     }
 
 //    删除下一个节点
@@ -56,5 +57,19 @@ public class Demo04Node {
         return this.node == null;
     }
 
+//    显示所有节点信息
+    public void show(){
+        Demo04Node currentNode = this;
+        System.out.print("[");
+        while(true){
+            if(currentNode.isLast()){
+                System.out.print(currentNode.getNum());
+                break;
+            }
+            System.out.print(currentNode.getNum() + ",");
+            currentNode = currentNode.node;
+        }
+        System.out.println("]");
+    }
 
 }
--- javaBasis/dataStructure/src/DataBasics/Demo04Test.java
+++ javaBasis/dataStructure/src/DataBasics/Demo04Test.java
@@ -5,20 +5,20 @@ public class Demo04Test {
         Demo04Node node1 = new Demo04Node(10);
         Demo04Node node2 = new Demo04Node(20);
         Demo04Node node3 = new Demo04Node(30);
-        Demo04Node node4 = new Demo04Node(40);
-        Demo04Node node5 = new Demo04Node(50);
 
-        node1.append(node2).append(node3).append(node4).append(node5);
+        node1.append(node2).append(node3);
 
-        System.out.println(node1.next().next().next().next().getNum());
+        node1.add(new Demo04Node(15));
+        node1.append(new Demo04Node(40));
+        node1.show();
+
+        System.out.println(node1.next().next().getNum());
 
         System.out.println(node1.isLast());
 
         node2.delNext();
         System.out.println(node1.next().next().getNum());
 
-
-//        int aaa = node1.append(node2).append(node3).getNum();
-//        System.out.println(aaa);
+        node1.append(node2);
     }
 }
