f3a801abe069321fc8caa3e321999010ba495cf7
Harry-wh <1093876434@qq.com>
2021-04-26 20:53:16 +0800

    21-4-26 mac

---
+++ JavaWeb/Code/javaweb-01-servlet/servlet-03-response/src/main/java/com/harry/servlet/ImageServlet.java
@@ -0,0 +1,60 @@
+package com.harry.servlet;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Random;
+
+public class ImageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //让浏览器2秒自动刷新一次
+        resp.setHeader("refresh","2");
+
+        //在内存中创建一个图片
+        BufferedImage image = new BufferedImage(100, 20, BufferedImage.TYPE_INT_BGR);
+        //得到图片
+        Graphics2D g = (Graphics2D) image.getGraphics();//笔
+        //设置图片的背景颜色
+        g.setColor(Color.white);
+        g.fillRect(0,0,100,30);
+        //给图片写数据
+        g.setColor(Color.BLUE);
+        g.setFont(new Font(null,Font.BOLD,20));
+        g.drawString(makeNum(),0,20);
+
+        //告诉浏览器，该请求用浏览器的方式打开
+        resp.setContentType("image/jpeg");
+        //网站存在缓存，不让浏览器缓存
+        resp.setDateHeader("expires",-1);
+        resp.setHeader("Cache-Control","no-cache");
+        resp.setHeader("Pragma","no-cache");
+
+        //把图片写给浏览器
+        ImageIO.write(image, "jpg", resp.getOutputStream());
+
+    }
+
+
+    //生成随机数
+    private String makeNum(){
+        Random r = new Random();
+        String num = r.nextInt(99999999) + "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 8 - num.length(); i++) {
+            sb.append("0");
+        }
+        num = sb.toString() + num;
+        return num;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
