e26c3a8223aa56237ea993f743b07f97bae796c1
Harry-wh <1093876434@qq.com>
2022-06-01 23:50:44 +0800

    22-6-1

--- JavaWeb/Code/Vue/myvue/src/main.js
+++ JavaWeb/Code/Vue/myvue/src/main.js
@@ -1,11 +1,11 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use()
+
 new Vue({
   el: '#app',
   components: { App },
--- JavaWeb/Code/Vue/myvue/src/router/index.js
+++ JavaWeb/Code/Vue/myvue/src/router/index.js
@@ -1,3 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+//安装路由
+Vue.use(VueRouter);
+
+//
